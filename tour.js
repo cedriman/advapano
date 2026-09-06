5d088524583f0861f23eca769969149a232bd243
cedriman <senzocharlie7@gmail.com>
2026-09-06 12:19:51 +0200

    Copy all contents from panoview repository

---
+++ tour.js
@@ -0,0 +1,5 @@
+/*
+	krpano Embedding Script
+	krpano 1.19-pr16 (build 2018-04-04)
+*/
+function embedpano(e){function ht(e){return(""+e).toLowerCase()}function pt(e,t){return e[g](t)>=0}function dt(){var t,r,i,s,o,u,a,f,l,c=n.location;c=c.search;if(c){t=".html5.flash.wmode.mobilescal[...](Content truncated for brevity - this is a large minified file)
